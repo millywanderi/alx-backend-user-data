a215e3008f8f43b65e991ad672a5e3ce4deeb56c
millywanderi <wanderinyambura5@gmail.com>
2024-02-13 15:54:51 +0300

    added more features

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Class auth"""
 
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -10,12 +11,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns false if the path is in included paths"""
-        if path is not None:
-            if not path.endswith("/"):
-                path = path + "/"
-
-            if path in excluded_paths:
-                return False
+        if path is not None and excluded_paths is not None:
+            for exclude_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclude_path[-1] == '*':
+                    pattern = '{}.*'.format(exclude_path[0:-1])
+                elif exclude_path[-1] == '/':
+                    pattern = '{}/*'.format(exclude_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclude_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
