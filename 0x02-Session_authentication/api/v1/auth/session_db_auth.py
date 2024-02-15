ceddddfe17134036ffd7544e86c16475dfa26f4a
millywanderi <wanderinyambura5@gmail.com>
2024-02-15 23:47:11 +0300

    Sessions in database

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Module for session database authentication
+"""
+from datetime import datetime, timedelta
+from models.user_session import UserSession
+from .session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """Session authentication class with db storage & expiration support
+    """
+    def create_session(self, user_id: str) -> str:
+        """Creates and stores id for the user"""
+        session_id = supper().create_session(user_id)
+
+        # Check if a session id is returned
+        if isinstance(session_id, str):
+            # Create a new UserSession instance and store it
+            kwargs = {
+                'user_id': user_id,
+                'session_id': session_id,
+            }
+            user_session = UserSession(**kwargs)
+            user_session.save()
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str) -> str:
+        """Retrieves the user id of the user associated with given
+        session id.
+        """
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return None
+        if len(sessions) <= 0:
+            return None
+
+        # Check the expiration time of the session
+        cur_time = datetime.now()
+        time_span = timedelta(seconds=self.session_duration)
+        exp_time = sessions[0].created_at + time_span
+        if exp_time < cur_time:
+            return None
+
+        # Return the user id associated with the session
+        return sessions[0].user_id
+
+    def destroy_session(self, request=None) -> bool:
+        """Destroys an authenticated session"""
+        session_id = self.session_cookie(request)
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return False
+        if len(sessions) <= 0:
+            return False
+        sessions[0].remove()
+        return True
