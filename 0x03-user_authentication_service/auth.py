3669d2ba5fcd88b21fd338149851db4121ad656f
millywanderi <wanderinyambura5@gmail.com>
2024-02-21 23:55:34 +0300

    added a new user to the db

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,3 +13,19 @@ from user import User
 def _hash_password(password: str) -> bytes:
     """method that hashes a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        """Method that initializes a new Auth instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method that authenicates user registration"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
